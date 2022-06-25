85c78d5a2556261356589680394ceecff103d17d
Joao Suza <joao.souza3157@gmail.com>
2022-06-25 12:32:35 -0300

    Criando tela router para poder incluir as rotas das paginas, implementação do graphql na tela de video e concluindo a tela de evento(tela principal)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Events } from "./pages/Events"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 
 function App() {
   return (
-<div>
-    <Events/>
-</div>
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+        <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
   )
 }
 
